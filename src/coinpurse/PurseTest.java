c2d4fc6c06fbf4c70c7377c2838d4987915ff34e
France98 <larn_francisza@hotmail.com>
2017-02-24 19:29:15 +0700

    remove error

--- src/coinpurse/PurseTest.java
+++ src/coinpurse/PurseTest.java
@@ -89,7 +89,7 @@ public class PurseTest {
 	public void testInsertSameCoin()
 	{
 		Purse purse = new Purse(5);
-		Coin coin = new Coin(10);
+		Valuable coin = new Coin(10);
 		assertTrue( purse.insert(coin) );
 		assertTrue( purse.insert(coin) ); // should be allowed
 		assertTrue( purse.insert(coin) ); // should be allowed
@@ -100,10 +100,10 @@ public class PurseTest {
 		Purse purse = new Purse(10);
 		int [] values = {1, 10, 1000};
 		for(int value : values) {
-			Coin coin = new Coin(value);
+			Valuable coin = new Coin(value);
 			assertTrue(purse.insert(coin));
 			assertEquals(value,  purse.getBalance(), TOL);
-			Coin [] result = purse.withdraw(value);
+			Valuable [] result = purse.withdraw(value);
 			assertTrue( result != null );
 			assertEquals( 1, result.length );
 			assertSame(  coin, result[0] );
@@ -127,10 +127,10 @@ public class PurseTest {
 		}
 		assertEquals(amount1+amount2, purse.getBalance(), TOL );
 		assertEquals(10, purse.count() );
-		Coin [] wd1 = purse.withdraw(amount1);
+		Valuable[] wd1 = purse.withdraw(amount1);
 		assertEquals(amount1, sumValue(wd1), TOL );
 		assertEquals(amount2, purse.getBalance(), TOL );
-		Coin [] wd2 = purse.withdraw(amount2);
+		Valuable[] wd2 = purse.withdraw(amount2);
 		assertEquals(0, purse.getBalance(), TOL );
 	}
 
@@ -148,13 +148,13 @@ public class PurseTest {
 	
 	/**
 	 * Sum the value of some coins.
-	 * @param coins array of coins
+	 * @param wd1 array of coins
 	 * @return sum of values of the coins
 	 */
-	private double sumValue(Coin [] coins)  {
-		if (coins == null) return 0;
+	private double sumValue(Valuable[] wd1)  {
+		if (wd1 == null) return 0;
 		double sum = 0;
-		for(Coin c: coins) if (c != null) sum += c.getValue();
+		for(Valuable c: wd1) if (c != null) sum += c.getValue();
 		return sum;
 	}
 }
